8c4cd07ae9fea5b61ea447fe0f84eb100dac65d8
LEVEL_cat <stalkerclearsky2000@gmail.com>
2023-05-17 15:00:24 +0300

    First Working App

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,7 +7,8 @@ namespace WebApp
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.RunAsync();
 
