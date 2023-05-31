5511ac94f36f8fb6fd51103f948d8d22c40b337a
LEVEL_cat <stalkerclearsky2000@gmail.com>
2023-05-31 13:49:13 +0300

    Alpha 0.2 Build

---
+++ WebApp/Controllers/RconUpdaterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.DbContexts;
+
+namespace WebApp.Controllers
+{
+    public class RconUpdaterController : Controller
+    {
+        public IActionResult UpdateStatDb([FromServices] StatisticDbContext dbContext)
+        {
+            return View(Json(dbContext.Servers));
+        }
+
+        public string Check()
+        {
+            return "Hello Updater!!";
+        }
+    }
+}
