5e4d6007f628f6cd48b2257534b40d3b1b5593fa
LEVEL_cat <stalkerclearsky2000@gmail.com>
2023-07-10 21:17:32 +0300

    last fix context

--- DbLibrary/StatisticModel/ServerType.cs
+++ DbLibrary/StatisticModel/ServerType.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DbLibrary.StatisticModel
 {
     [Table("servertype")]
     public class ServerType
     {
+        [Key]
+        [Column("ServerTypeID")]
+        public byte ID { get; set; }
 
+        [Column("Type")]
+        public string? Type { get; set; }
     }
 }
