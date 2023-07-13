e6572c028586db4214cbad91588474c545fbe3e6
LEVEL_cat <stalkerclearsky2000@gmail.com>
2023-07-13 23:33:09 +0300

    Added TestModule and InteractivityConfigurator

--- DiscordBotApp/DiscordConfigurator.cs
+++ DiscordBotApp/DiscordConfigurator.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Interactivity;
 
 namespace DiscordApp
 {
@@ -12,9 +13,18 @@ namespace DiscordApp
                 {
                     Token = "token",
                     TokenType = TokenType.Bot,
-                    Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
-                    MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+                    //Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+                    Intents = DiscordIntents.All,
+                    MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Information,
                     LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt"
+                };
+            }
+
+            public static InteractivityConfiguration GetInteractivityConfiguration()
+            {
+                return new InteractivityConfiguration()
+                {
+                    
                 };
             }
         }
